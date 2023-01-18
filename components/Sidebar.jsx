368a1827a6eef91d89024841a16e07aa3e252235
Mauro.V <remanomauro@gmail.com>
2023-01-18 05:59:05 +0100

    sidebar setup

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-import {RxSketchLogo} from 'react-icons/rx'
+import {RxSketchLogo, RxDashboard, RxPerson} from 'react-icons/rx'
+import {HiOutlineShoppingBag} from 'react-icons/hi'
+import {FiSettings} from 'react-icons/fi'
 
 const Sidebar = ({children}) => {
 return (
@@ -11,8 +13,29 @@ return (
         <div className='fixed w-20 h-screen bg-white border-r-[1px] flex flex-col justify-between'>
             <div className='flex flex-col items-center'>
                 <Link href='/'>
-                    <div>
+                    <div className="bg-purple-800 text-white p-3 rounded-lg inline-block my-4">
                         <RxSketchLogo size={20} />
+                    </div>
+                </Link>
+                <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
+                <Link href='/'>
+                    <div className="bg-gray-100 hover:bg-gray-300 cursor-pointer my-4 p-3 rounded-lg inline-block">
+                        <RxDashboard size={20} />
+                    </div>
+                </Link>
+                <Link href='/customers'>
+                    <div className="bg-gray-100 hover:bg-gray-300 cursor-pointer my-4 p-3 rounded-lg inline-block">
+                        <RxPerson size={20} />
+                    </div>
+                </Link>
+                <Link href='/orders'>
+                    <div className="bg-gray-100 hover:bg-gray-300 cursor-pointer my-4 p-3 rounded-lg inline-block">
+                        <HiOutlineShoppingBag size={20} />
+                    </div>
+                </Link>
+                <Link href='/'>
+                    <div className="bg-gray-100 hover:bg-gray-300 cursor-pointer my-4 p-3 rounded-lg inline-block">
+                        <FiSettings size={20} />
                     </div>
                 </Link>
             </div>
