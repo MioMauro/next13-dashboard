8782461ee900765ab262f73a5801042c449aaf77
Mauro.V <remanomauro@gmail.com>
2023-01-18 10:18:18 +0100

    rendering chart data

--- components/BarChart.jsx
+++ components/BarChart.jsx
@@ -26,6 +26,22 @@ const BarChart = () => {
     })
     const [chartOptions, setChartOptions] = useState({})
 
+    useEffect(() => {
+        setChartData({
+            labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+            datasets: [
+                {
+                    label: 'Sales $',
+                    data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+                    borderColor: 'rgb(53, 162, 253)',
+                    backgroundColor: 'rgba(53, 162, 235, 0.4)',
+
+                },
+            ],
+        })
+
+    }, [])
+
 
 return (
     <>
