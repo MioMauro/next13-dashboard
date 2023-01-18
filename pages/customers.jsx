bce5e729b5f798ae2ed72111802b4265be6859e7
Mauro.V <remanomauro@gmail.com>
2023-01-18 15:45:57 +0100

    orders page

--- pages/customers.jsx
+++ pages/customers.jsx
@@ -7,7 +7,7 @@ const customers = () => {
     <div className='bg-gray-100 min-h-screen'>
         <div className='flex justify-between p-4'>
         <h2>Customers</h2>
-        <h2>Welcome Back, Clint</h2>
+        <h2>Welcome Back, Mauro</h2>
         </div>
         <div className='p-4'>
         <div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto'>
@@ -26,8 +26,8 @@ const customers = () => {
                         </div>
                         <p className='pl-4'>{order.name.first + ' ' + order.name.last}</p>
                     </div>
-                    <p className='text-gray-600 sm:text-left text-right'>{order.name.first}@gmail.com</p>
-                    <p className='hidden md:flex'>{order.date}</p>
+                        <p className='text-gray-600 sm:text-left text-right'>{order.name.first}@gmail.com</p>
+                        <p className='hidden md:flex'>{order.date}</p>
                     <div className='sm:flex hidden justify-between items-center'>
                         <p>{order.method}</p>
                         <BsThreeDotsVertical />
